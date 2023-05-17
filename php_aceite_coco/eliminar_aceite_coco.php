65b5a596993a7ac60a811eeba4e8eacfd59ffaa6
Nala1307 <133907923+Nala1307@users.noreply.github.com>
2023-05-17 15:53:31 -0400

    Add files via upload

---
+++ php_aceite_coco/eliminar_aceite_coco.php
@@ -0,0 +1,10 @@
+<?php
+$id_nutriente = $_GET['id'];
+
+$conexion = mysqli_connect("localhost", "root", "", "foodmp");
+$consulta = "DELETE FROM aceite_coco WHERE id_nutriente = '$id_nutriente'";
+$rta = mysqli_query($conexion, $consulta);
+header("Location: aceite_coco.php");
+?>
+
+
---
+++ php_galletas_jengibre/eliminar_galletas_jengibre.php
@@ -0,0 +1,10 @@
+<?php
+$id_nutriente = $_GET['id'];
+
+$conexion = mysqli_connect("localhost", "root", "", "foodmp");
+$consulta = "DELETE FROM galletas_jengibre WHERE id_nutriente = '$id_nutriente'";
+$rta = mysqli_query($conexion, $consulta);
+header("Location: galletas_jengibre.php");
+?>
+
+
